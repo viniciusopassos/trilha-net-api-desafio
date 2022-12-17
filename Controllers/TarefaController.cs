b278a8f4717661553a1a4e7ffb6cccaa0d4f779d
viniciusOP <viniciusopassos@hotmail.com>
2022-12-17 10:10:26 -0300

    feat: Criar HttpPut AtualizarTarefa

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -81,7 +81,7 @@ namespace TrilhaApiDesafio.Controllers
             _context.Add(tarefa);
 
             _context.SaveChanges();
-            
+
             return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
         }
 
@@ -98,7 +98,16 @@ namespace TrilhaApiDesafio.Controllers
 
             // TODO: Atualizar as informações da variável tarefaBanco com a tarefa recebida via parâmetro
             // TODO: Atualizar a variável tarefaBanco no EF e salvar as mudanças (save changes)
-            return Ok();
+            
+            tarefaBanco.Titulo = tarefa.Titulo;
+            tarefaBanco.Descricao = tarefa.Descricao;
+            tarefaBanco.Data = tarefa.Data;
+            tarefaBanco.Status = tarefa.Status;
+
+            _context.Tarefas.Update(tarefaBanco);
+            _context.SaveChanges();
+
+            return Ok(tarefaBanco);
         }
 
         [HttpDelete("{id}")]
